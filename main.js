e7950fca90e989b41a064630dc11b4bb91405377
AntoniyaBK <antoniak12@abv.bg>
2025-12-23 14:34:47 +0200

    Initial commit with Electron for Web App to Desktop app

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
+
+  // Add URL for your Web Instance
+  win.loadURL('https://your-web.com'); 
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
